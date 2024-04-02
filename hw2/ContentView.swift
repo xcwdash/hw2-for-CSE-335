b1bc61101aea92ff02f295061250018279801d9e
Zachary <zach.lee96@gmai.com>
2024-04-02 02:06:06 -0700

    Initial Commit

---
+++ hw2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  hw2
+//
+//  Created by Zachary Lee on 4/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ hw2/hw2App.swift
@@ -0,0 +1,17 @@
+//
+//  hw2App.swift
+//  hw2
+//
+//  Created by Zachary Lee on 4/2/24.
+//
+
+import SwiftUI
+
+@main
+struct hw2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
